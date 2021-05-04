f9f4f3158b197e1d03c155181f8bf9c2b471754f
Wahyu Widiastono <widiastono@crx.solonet.net.id>
2021-05-05 06:02:15 +0700

    tanpa install laravel, karena sudah include

--- install.sh
+++ install.sh
@@ -5,21 +5,22 @@ GROUP=widiastono
 USERID=`id -u $USER`
 GROUPID=`id -g $GROUP`
 
+### Source File sudah ada di repo
 ### Source Application
-mkdir src
+#mkdir src
 
 ### install dependencies laravel
-cd src
+#cd src
 
-docker run -u $USERID:$GROUPID --rm -v $(pwd):/app composer create-project --prefer-dist laravel/laravel projects
-cp -rpv projects/.* .
+#docker run -u $USERID:$GROUPID --rm -v $(pwd):/app composer create-project --prefer-dist laravel/laravel projects
+#cp -rpv projects/.* .
 
-rm -rf projects
+#rm -rf projects
 
 #docker-compose up --build -d 
 docker-compose up -d 
 
-cp .env.example .env
+#cp .env.example .env
 
 docker-compose exec -u $USERID:$GROUPID app php artisan key:generate
 docker-compose exec -u $USERID:$GROUPID app php artisan config:cache
