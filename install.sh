0260e9dd6ec52acac2ab79d40231a974eb791f9a
Wahyu Widiastono <widiastono@crx.solonet.net.id>
2021-05-05 05:55:06 +0700

    tanpa building, pakai image dari l7

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USER=widiastono
+GROUP=widiastono
+
+USERID=`id -u $USER`
+GROUPID=`id -g $GROUP`
+
+### Source Application
+mkdir src
+
+### install dependencies laravel
+cd src
+
+docker run -u $USERID:$GROUPID --rm -v $(pwd):/app composer create-project --prefer-dist laravel/laravel projects
+cp -rpv projects/.* .
+
+rm -rf projects
+
+#docker-compose up --build -d 
+docker-compose up -d 
+
+cp .env.example .env
+
+docker-compose exec -u $USERID:$GROUPID app php artisan key:generate
+docker-compose exec -u $USERID:$GROUPID app php artisan config:cache
